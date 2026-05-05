42ad2dd9deb15632aedeae6d189f1cc36de4f228
heshangoonawardena <heshangoonawardena@gmail.com>
2026-05-05 10:34:20 +0530

    chore(styles): update colors

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,17 @@
 import { House } from "lucide-react";
-
-import { LoginForm } from "@/app/(auth)/login/_components/login-form";
+import { LoginForm } from "./_components/login-form";
+import Link from "next/link";
 
 export default function LoginPage() {
 	return (
-		<div className="bg-neutral-100 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 dark:bg-neutral-800">
+		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 dark:bg-neutral-00">
 			<div className="flex w-full max-w-sm flex-col gap-6">
-				<a href="#" className="flex items-center gap-2 self-center font-medium">
-					<div className="bg-neutral-900 text-neutral-50 flex size-6 items-center justify-center rounded-md dark:bg-neutral-50 dark:text-neutral-900">
-						<House className="size-4" />
+				<Link href="#" className="flex items-center gap-2 self-center font-medium">
+					<div className="bg-primary text-primary-foreground flex size-9 items-center justify-center rounded-md ">
+						<House />
 					</div>
 					rently
-				</a>
+				</Link>
 				<LoginForm />
 			</div>
 		</div>
