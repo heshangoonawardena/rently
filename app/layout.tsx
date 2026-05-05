872aa4ba768c4bb25562d826899a57c6b76e91f0
heshangoonawardena <heshangoonawardena@gmail.com>
2026-05-05 16:09:30 +0530

    chore(nextjs): update next js to V16

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className="scroll-smooth" suppressHydrationWarning>
+		<html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
